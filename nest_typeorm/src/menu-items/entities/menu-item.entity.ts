fd55807750489e6a74cd0db52bba26b6c1658d82
ahmad najar <ahmadnajar10@gmail.com>
2022-10-07 01:55:58 +0300

    add the logic to the code

--- nest_typeorm/src/menu-items/entities/menu-item.entity.ts
+++ nest_typeorm/src/menu-items/entities/menu-item.entity.ts
@@ -1,19 +1,26 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn} from 'typeorm';
 
 @Entity()
 export class MenuItem {
-  @PrimaryGeneratedColumn()
-  id: number;
+    @PrimaryGeneratedColumn()
+    id: number;
 
-  @Column()
-  name: string;
+    @Column()
+    name: string;
 
-  @Column()
-  url: string;
+    @Column()
+    url: string;
 
-  @Column({ type: 'integer', default: null })
-  parentId: number;
+    @Column({type: 'integer', default: null})
+    parentId: number;
 
-  @Column({ type: 'datetime' })
-  createdAt: string;
+    @Column({type: 'datetime'})
+    createdAt: string;
+
+    @OneToMany(() => MenuItem, (menuItem) => menuItem.parent)
+    @JoinColumn({name: 'parentId'})
+    children: MenuItem[];
+
+    @ManyToOne(() => MenuItem, (menuItem) => menuItem.children)
+    parent: MenuItem;
 }
